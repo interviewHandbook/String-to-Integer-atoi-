1d68235ab8ed81b5ff3b88dac8af54382bf792da
interviewHandbook <158495810+interviewHandbook@users.noreply.github.com>
2025-07-29 13:20:59 +0530

    first commit
    first upoad

---
+++ FirstApproach.java
@@ -0,0 +1,86 @@
+package com.interviewprep.leetcode.medium.stringtointeger;
+
+public class FirstApproach {
+
+	public static void main(String[] args) {
+		
+		FirstApproach fa = new FirstApproach();
+		//int strToInt = fa.myAtoi("-91283472332");
+		//int strToInt = fa.myAtoi("3.14159");
+		int strToInt = fa.myAtoi("words and 987");
+		//int strToInt = fa.myAtoi(".1");
+		//int strToInt = fa.myAtoi("+1");
+		//int strToInt = fa.myAtoi("  -042");
+		//int strToInt = fa.myAtoi("+-12");
+		
+		System.out.println(strToInt);
+	}
+	
+	
+	
+	public int myAtoi(String s) {
+
+		int strToInt = 0;
+
+		s = s.strip();
+		int len = s.length();	
+		StringBuilder str = new StringBuilder();
+		boolean isNeg = false;
+
+		for(int i=0; i<len; i++) {
+
+			Character c = s.charAt(i);
+
+			if(i == 0 &&  (c == '-'  ||  c == '+') ) {
+
+				if(c == '-')
+					isNeg = true;
+
+				continue;
+			}
+			if(Character.isDigit(c)  ) {
+
+				str = str.append( Character.toString(c) );
+
+				continue;
+			}
+
+
+			if(i > 0 &&  (c == '-'  ||  c == '+') ) {
+
+				break;
+			}
+
+			if(!Character.isDigit(c) ) {
+
+				break;
+			}
+
+		} // endfor
+
+
+
+		if(!str.isEmpty()) {
+			try {
+				strToInt = Integer.valueOf(str.toString());
+			}
+			catch(NumberFormatException nfe) {
+
+				if(isNeg)
+					return Integer.MIN_VALUE;
+				else
+					return Integer.MAX_VALUE;
+			}
+
+		}
+
+
+		if(isNeg) {
+			strToInt = strToInt * -1;
+		}
+
+
+		return strToInt;
+	}	
+
+}

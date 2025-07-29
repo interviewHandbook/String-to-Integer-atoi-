32d8e049a12eb3106676ac9f6e2df2b6557d9c01
interviewHandbook <158495810+interviewHandbook@users.noreply.github.com>
2025-07-29 13:37:17 +0530

    Update FirstApproach.java
    second commit - removed unnecessary checks and added comments

--- FirstApproach.java
+++ FirstApproach.java
@@ -3,7 +3,7 @@ package com.interviewprep.leetcode.medium.stringtointeger;
 public class FirstApproach {
 
 	public static void main(String[] args) {
-		
+
 		FirstApproach fa = new FirstApproach();
 		//int strToInt = fa.myAtoi("-91283472332");
 		//int strToInt = fa.myAtoi("3.14159");
@@ -12,12 +12,12 @@ public class FirstApproach {
 		//int strToInt = fa.myAtoi("+1");
 		//int strToInt = fa.myAtoi("  -042");
 		//int strToInt = fa.myAtoi("+-12");
-		
+
 		System.out.println(strToInt);
 	}
-	
-	
-	
+
+
+
 	public int myAtoi(String s) {
 
 		int strToInt = 0;
@@ -33,24 +33,21 @@ public class FirstApproach {
 
 			if(i == 0 &&  (c == '-'  ||  c == '+') ) {
 
+				// if '-' is the first char then turn the flag to true
 				if(c == '-')
 					isNeg = true;
 
 				continue;
 			}
+
+			// if char is digit then append to string
 			if(Character.isDigit(c)  ) {
 
 				str = str.append( Character.toString(c) );
-
 				continue;
 			}
 
-
-			if(i > 0 &&  (c == '-'  ||  c == '+') ) {
-
-				break;
-			}
-
+			// if char is not a digit then break
 			if(!Character.isDigit(c) ) {
 
 				break;
@@ -59,11 +56,12 @@ public class FirstApproach {
 		} // endfor
 
 
-
+		// convert string to integer
 		if(!str.isEmpty()) {
 			try {
 				strToInt = Integer.valueOf(str.toString());
 			}
+			// if val is too large for int then return the min / max int val
 			catch(NumberFormatException nfe) {
 
 				if(isNeg)
@@ -74,11 +72,10 @@ public class FirstApproach {
 
 		}
 
-
+		// if flag is true then multiply by -1
 		if(isNeg) {
 			strToInt = strToInt * -1;
 		}
-
 
 		return strToInt;
 	}	
